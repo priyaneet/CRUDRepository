cbabfb61dde9c6dabc3e458e6ac0d2ae16bff838
priyaneet bhatia <priyaneet2800@gmail.com>
2026-02-18 22:31:30 +0530

    Add project files.

---
+++ CRUDWebApplication/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUDWebApplication.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUDWebApplication/Models/EFCoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Metrics;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWebApplication.Models
+{
+    public class EFCoreDbContext:DbContext
+    {
+        //Constructor calling the Base DbContext Class Constructor
+        public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options) : base(options)
+        {
+        }
+        //OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        //Adding Domain Classes as DbSet Properties
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
